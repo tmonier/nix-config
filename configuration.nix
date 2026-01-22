199afe68ae6297f47d033397b51f9540c8e49f0a
tmonier <tmonier03@gmail.com>
2026-01-22 02:45:15 +0000

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -10,20 +10,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos"; 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Chicago";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -38,11 +30,8 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.ly.enable = true;
   services.desktopManager.plasma6.enable = true;
   hardware.bluetooth = {
@@ -50,33 +39,22 @@
   powerOnBoot = true;
   settings = {
     General = {
-      # Shows battery charge of connected devices on supported
-      # Bluetooth adapters. Defaults to 'false'.
       Experimental = true;
-      # When enabled other devices can connect faster to us, however
-      # the tradeoff is increased power consumption. Defaults to
-      # 'false'.
       FastConnectable = true;
     };
     Policy = {
-      # Enable all controllers when they are found. This includes
-      # adapters present on start as well as adapters that are plugged
-      # in later on. Defaults to 'true'.
       AutoEnable = true;
     };
   };
 };
 services.blueman.enable = true;
 programs.zsh.enable = true;
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -84,18 +62,9 @@ programs.zsh.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tmonier = {
     isNormalUser = true;
     description = "tmonier";
@@ -111,6 +80,13 @@ programs.zsh.enable = true;
 
   nixpkgs.config.allowUnfree = true;
 
+ # Emacs
+  services.emacs = {
+  enable = true;
+  defaultEditor = true;
+  package = pkgs.emacs-pgtk;
+ };
+
   environment.systemPackages = with pkgs; [
     vim 
     wget
@@ -119,7 +95,6 @@ programs.zsh.enable = true;
     steam 
     nnn 
     gcc 
-    clang
     neovim
     alacritty
     fd
